c1fda0604c267cd7fba43dc8cb8cc37548159313
HauWibuDz <105596721+HauWibuDz@users.noreply.github.com>
2022-05-15 21:46:28 +0700

    Add files via upload

---
+++ bt1/dist/index.js
@@ -0,0 +1,12 @@
+var person = {
+    name: "Nguyen Hoang Anh",
+    email: "hoanganh@gmail.com",
+    phone: "0986421127",
+    gender: "nam",
+    favour: "Xem phim, doc sach, du lich, bong da"
+};
+document.getElementById("name").innerHTML = person.name;
+document.getElementById("email").innerHTML = person.email;
+document.getElementById("telephone").innerHTML = person.phone;
+document.getElementById("gender").innerHTML = person.gender;
+document.getElementById("favour").innerHTML = person.favour;
